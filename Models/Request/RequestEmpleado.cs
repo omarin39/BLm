acf271e51590cd6370f7cf4b20f12781a8831971
omarin39 <omaralvarez01.oaa@gmail.com>
2021-05-04 00:00:00 -0500

    Cambios en API

--- Models/Request/RequestEmpleado.cs
+++ Models/Request/RequestEmpleado.cs
@@ -9,12 +9,12 @@ namespace APIRestV2.Models.Request
     public class RequestEmpleado
     {  
         public long IdEmpleado { get; set; }
-        public string NNomina { get; set; }
+        public string NumeroNomina { get; set; }
         public string Nombre { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
-        public DateTime FNacimiento { get; set; }
-        public DateTime FIngreso { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public DateTime FechaIngreso { get; set; }
         public string Email { get; set; }
         public string NominaJefe { get; set; }
         public Nullable<long> DepartamentoIdDepartamentoNivel0 { get; set; }
